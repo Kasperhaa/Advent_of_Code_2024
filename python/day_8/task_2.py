fe38cddc054e7a8386def1d35b510aa4be0c7aa7
Kasper Haaberg <kaspehaa@ntnu.no>
2024-12-11 01:17:34 +0100

    Added solution with simple DEBUG function for day 8!

--- python/day_8/task_2.py
+++ python/day_8/task_2.py
@@ -27,19 +27,26 @@ def solve(file_path: str) -> int:
     for frequency_pos in antennas_pos.values():
         for idx, pos in enumerate(frequency_pos):
             for other_pos in frequency_pos[idx+1:]:
-                diff = other_pos - pos
-                antinode = np.array([pos-diff, other_pos+diff])
-                
-                for x, y in antinode:
-                    if x < 0 or y < 0 or x > shape[0] or y > shape[1]:
-                        if DEBUG:
+                difference_direction = np.array([pos - other_pos, other_pos - pos])
+                antinode = np.array([pos, other_pos])
+                print(*zip(antinode, difference_direction))
+                for (x,y), (x_diff, y_diff) in zip(antinode, difference_direction):
+                    out_of_bounds = lambda x,y,shape: (x < 0 or y < 0 or x > shape[0] or y > shape[1]) 
+                    if DEBUG:
+                        if out_of_bounds(x,y,shape):
                             print(f"for {tuple(pos)} and {tuple(other_pos)} :")
                             print(f"{tuple(antinode)} out of bounds!")
-                            print(f"{tuple(diff)}")
-                        continue
+                            print(f"{tuple(difference_direction)}")
+                            continue
+
+                    while not out_of_bounds(x,y,shape):
+                        if DEBUG:
+                            antinodes_array[x][y] = "#" # Only for visualization
+                        antinodes_pos[f"{x},{y}"] = 1
+                        x += x_diff
+                        y += y_diff
                     if DEBUG:
-                        antinodes_array[x][y] = "#" # Only for visualization
-                    antinodes_pos[f"{x},{y}"] = 1
+                        print("reached end!")
 
     if DEBUG:
         for row in antinodes_array:
@@ -57,7 +64,7 @@ def calculate_time(func, *args):
     return result
 
 def main() -> int:
-    file = "python/day_8/sample.csv"
+    file = "python/day_8/input.csv"
     return calculate_time(solve, file)
 
 if __name__ == "__main__":
